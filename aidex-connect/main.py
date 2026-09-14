2a0b623fc3e3849fcced8aa11a34c85d5c3bb735
Gilcare <gilcare.com@gmail.com>
2026-09-14 15:12:26 +0200

    main.py

--- aidex-connect/main.py
+++ aidex-connect/main.py
@@ -27,7 +27,7 @@ from datetime import datetime, timedelta, timezone
 import httpx
 from fastapi import FastAPI, Request, HTTPException, Query
 from fastapi.responses import HTMLResponse, RedirectResponse
-from aidex-connect.routes import router as aidex_router 
+from aidex_connect.routes import router as aidex_router 
 from cryptography.fernet import Fernet
 from pymongo import MongoClient, ASCENDING
 
